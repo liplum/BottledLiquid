6f3b95c89920b512bf9220519c152471bdbc813e
liplum <Li_plum@outlook.com>
2022-06-19 22:21:10 +0800

    [AR] fixed name issue.

--- src/net/liplum/bl/bottle/Bottling.java
+++ src/net/liplum/bl/bottle/Bottling.java
@@ -3,6 +3,7 @@ package net.liplum.bl.bottle;
 import arc.struct.ObjectMap;
 import mindustry.Vars;
 import mindustry.type.Liquid;
+import net.liplum.bl.BottledLiquidMod;
 
 public class Bottling {
     public static ObjectMap<Liquid, BottledLiquid> liquid2Bottled = new ObjectMap<>();
@@ -11,12 +12,14 @@ public class Bottling {
      * Bottle all liquid except for the hidden.
      */
     public static void bottlingAllLiquid() {
+        Vars.content.setCurrentMod(BottledLiquidMod.self);
         for (Liquid liquid : Vars.content.liquids()) {
             if (!liquid.isHidden()) {
                 BottledLiquid bottled = new BottledLiquid(liquid);
                 liquid2Bottled.put(liquid, bottled);
             }
         }
+        Vars.content.setCurrentMod(null);
     }
 
     public static BottledLiquid get(Liquid liquidToBeBottled) {
